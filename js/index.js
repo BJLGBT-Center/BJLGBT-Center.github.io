801e19008caf5d0fd7aaf892fce80586c4af4fba
ZhongxuYang <podo521@qq.com>
2017-06-26 17:41:00 +0800

    new sky

--- js/index.js
+++ js/index.js
@@ -6,106 +6,195 @@
 //	setload1();
 //})();
 
-var canvas = document.getElementById('canvas'),
-	ctx = canvas.getContext('2d'),
-	w = canvas.width = window.innerWidth,
-	h = canvas.height = window.innerHeight,
+//星空
+//var canvas = document.getElementById('canvas'),
+//	ctx = canvas.getContext('2d'),
+//	w = canvas.width = window.innerWidth,
+//	h = canvas.height = window.innerHeight,
+//
+//	hue = 217,
+//	stars = [],
+//	count = 0,
+//	maxStars = 1100;                //星星数量,默认1300
+//var canvas2 = document.createElement('canvas'),
+//	ctx2 = canvas2.getContext('2d');
+//canvas2.width = 100;
+//canvas2.height = 100;
+//var half = canvas2.width / 2,
+//	gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+//gradient2.addColorStop(0.025, '#CCC');
+//gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
+//gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
+//gradient2.addColorStop(1, 'transparent');
+//
+//ctx2.fillStyle = gradient2;
+//ctx2.beginPath();
+//ctx2.arc(half, half, half, 0, Math.PI * 2);
+//ctx2.fill();
+//
+//// End cache
+//function random(min, max) {
+//	if (arguments.length < 2) {
+//		max = min;
+//		min = 0;
+//	}
+//
+//	if (min > max) {
+//		var hold = max;
+//		max = min;
+//		min = hold;
+//	}
+//
+//	return Math.floor(Math.random() * (max - min + 1)) + min;
+//}
+//
+//function maxOrbit(x, y) {
+//	var max = Math.max(x, y),
+//		diameter = Math.round(Math.sqrt(max * max + max * max));
+//	return diameter / 2;
+//	//星星移动范围，值越大范围越小，
+//}
+//
+//var Star = function() {
+//
+//	this.orbitRadius = random(maxOrbit(w, h));
+//	this.radius = random(60, this.orbitRadius) / 10;       //星星大小,值越大星星越小,默认8
+//
+//	this.orbitX = w / 2;
+//	this.orbitY = h / 2;
+//	this.timePassed = random(0, maxStars);
+//	this.speed = random(this.orbitRadius) / 80000;        //星星移动速度,值越大越慢,默认5W
+//
+//	this.alpha = random(2, 10) / 10;
+//
+//	count++;
+//	stars[count] = this;
+//}
+//
+//Star.prototype.draw = function() {
+//	var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+//		y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+//		twinkle = random(10);
+//
+//	if (twinkle === 1 && this.alpha > 0) {
+//		this.alpha -= 0.05;
+//	} else if (twinkle === 2 && this.alpha < 1) {
+//		this.alpha += 0.05;
+//	}
+//
+//	ctx.globalAlpha = this.alpha;
+//	ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+//	this.timePassed += this.speed;
+//}
+//
+//for (var i = 0; i < maxStars; i++) {
+//	new Star();
+//}
+//
+//function animation() {
+//	ctx.globalCompositeOperation = 'source-over';
+//	ctx.globalAlpha = 0.5;                                 //尾巴
+//	ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)';
+//	ctx.fillRect(0, 0, w, h)
+//
+//	ctx.globalCompositeOperation = 'lighter';
+//	for (var i = 1,
+//			 l = stars.length; i < l; i++) {
+//		stars[i].draw();
+//	};
+//
+//	window.requestAnimationFrame(animation);
+//}
+//
+//animation();
 
-	hue = 217,
-	stars = [],
-	count = 0,
-	maxStars = 1100;                //星星数量,默认1300
-var canvas2 = document.createElement('canvas'),
-	ctx2 = canvas2.getContext('2d');
-canvas2.width = 100;
-canvas2.height = 100;
-var half = canvas2.width / 2,
-	gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
-gradient2.addColorStop(0.025, '#CCC');
-gradient2.addColorStop(0.1, 'hsl(' + hue + ', 61%, 33%)');
-gradient2.addColorStop(0.25, 'hsl(' + hue + ', 64%, 6%)');
-gradient2.addColorStop(1, 'transparent');
 
-ctx2.fillStyle = gradient2;
-ctx2.beginPath();
-ctx2.arc(half, half, half, 0, Math.PI * 2);
-ctx2.fill();
+//////////////////////////////////////////////////////////
+//方块星空
+window.requestAnimFrame = (function() {
+	return window.requestAnimationFrame
+})();
+var canvas = document.getElementById("canvas");
+var c = canvas.getContext("2d");
 
-// End cache
-function random(min, max) {
-	if (arguments.length < 2) {
-		max = min;
-		min = 0;
+var numStars = 1900;
+var radius = '0.' + Math.floor(Math.random() * 9) + 1;
+var focalLength = canvas.width * 2;
+var warp = 0;
+var centerX, centerY;
+
+var stars = [],
+	star;
+var i;
+
+var animate = true;
+
+initializeStars();
+
+function executeFrame() {
+
+	if (animate) requestAnimFrame(executeFrame);
+	moveStars();
+	drawStars();
+}
+
+function initializeStars() {
+	centerX = canvas.width / 2;
+	centerY = canvas.height / 2;
+
+	stars = [];
+	for (i = 0; i < numStars; i++) {
+		star = {
+			x: Math.random() * canvas.width,
+			y: Math.random() * canvas.height,
+			z: Math.random() * canvas.width,
+			o: '0.' + Math.floor(Math.random() * 99) + 1
+		};
+		stars.push(star);
 	}
+}
 
-	if (min > max) {
-		var hold = max;
-		max = min;
-		min = hold;
+function moveStars() {
+	for (i = 0; i < numStars; i++) {
+		star = stars[i];
+		star.z--;
+
+		if (star.z <= 0) {
+			star.z = canvas.width;
+		}
 	}
-
-	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function maxOrbit(x, y) {
-	var max = Math.max(x, y),
-		diameter = Math.round(Math.sqrt(max * max + max * max));
-	return diameter / 2;
-	//星星移动范围，值越大范围越小，
-}
+function drawStars() {
+	var pixelX, pixelY, pixelRadius;
 
-var Star = function() {
-
-	this.orbitRadius = random(maxOrbit(w, h));
-	this.radius = random(60, this.orbitRadius) / 10;       //星星大小,值越大星星越小,默认8
-
-	this.orbitX = w / 2;
-	this.orbitY = h / 2;
-	this.timePassed = random(0, maxStars);
-	this.speed = random(this.orbitRadius) / 80000;        //星星移动速度,值越大越慢,默认5W
-
-	this.alpha = random(2, 10) / 10;
-
-	count++;
-	stars[count] = this;
-}
-
-Star.prototype.draw = function() {
-	var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
-		y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
-		twinkle = random(10);
-
-	if (twinkle === 1 && this.alpha > 0) {
-		this.alpha -= 0.05;
-	} else if (twinkle === 2 && this.alpha < 1) {
-		this.alpha += 0.05;
+	if (canvas.width != window.innerWidth || canvas.width != window.innerWidth) {
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		initializeStars();
 	}
+	if (warp == 0) {
+		c.fillStyle = "rgba(0,10,20,1)";
+		c.fillRect(0, 0, canvas.width, canvas.height);
+	}
+	c.fillStyle = "rgba(255, 255, 255, " + radius + ")";
+	for (i = 0; i < numStars; i++) {
+		star = stars[i];
 
-	ctx.globalAlpha = this.alpha;
-	ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
-	this.timePassed += this.speed;
+		pixelX = (star.x - centerX) * (focalLength / star.z);
+		pixelX += centerX;
+		pixelY = (star.y - centerY) * (focalLength / star.z);
+		pixelY += centerY;
+		pixelRadius = 1 * (focalLength / star.z);
+
+		c.fillRect(pixelX, pixelY, pixelRadius, pixelRadius);
+		c.fillStyle = "rgba(255, 255, 255, " + star.o + ")";
+	}
 }
 
-for (var i = 0; i < maxStars; i++) {
-	new Star();
-}
+executeFrame();
 
-function animation() {
-	ctx.globalCompositeOperation = 'source-over';
-	ctx.globalAlpha = 0.5;                                 //尾巴
-	ctx.fillStyle = 'hsla(' + hue + ', 64%, 6%, 2)';
-	ctx.fillRect(0, 0, w, h)
 
-	ctx.globalCompositeOperation = 'lighter';
-	for (var i = 1,
-			 l = stars.length; i < l; i++) {
-		stars[i].draw();
-	};
-
-	window.requestAnimationFrame(animation);
-}
-
-animation();
 
 ///*--------做图片预加载--------*/
 //document.addEventListener('touchstart',function(ev){
